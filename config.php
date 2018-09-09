c2ef69b0410fa034a01f4005bb0c7d21ca035bf1
some-crap <43110604+some-crap@users.noreply.github.com>
2018-09-09 16:19:04 +0300

    Добавление/редактирование ДЗ
    Необходимо подставить свои значения в файлы index.php и config.php
    Для создания таблицы в базе данных требуется выполнить SQL запрос вручную (phpMyAdmin подойдёт)
    Запрос:
    
    CREATE TABLE 'BD_NAME'.'table_homework' ( 'id' INT NOT NULL AUTO_INCREMENT , 'timestamp' INT NOT NULL , 'subject' TEXT NOT NULL , 'hometask' TEXT NOT NULL , PRIMARY KEY (id)) ENGINE = InnoDB
    
    В запросе необходимо подставить имя своей БД

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+const HOST     =  "localhost";
+const USERNAME =  "DBUSER";
+const PASS     =  "DBPASS";
+const DBNAME   =  "DB";
+
+//Secure key which give access to adding / editing / deleting homework
+const ADMIN_TOKEN =  "Key";
+?>
