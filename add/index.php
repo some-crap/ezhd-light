d12b50d97b13c34897df551673f3ea7a2c08850b
some-crap <43110604+some-crap@users.noreply.github.com>
2018-09-11 21:19:08 +0300

    Форма добавления ДЗ

--- add/index.php
+++ add/index.php
@@ -8,7 +8,7 @@ echo'<head>
 	<script type="text/javascript" src="placeholder.js"></script>
 </head>
 <body>
-<form id="slick-login" action="action.php" method="post" enctype="multipart/form-data">
+<form id="slick-login" action="../addHomework.php" method="post" enctype="multipart/form-data">
 	<label for="password">Токен</label><input type="password" name="admin_token" class="placeholder" placeholder="token">
     <label for="username">Предмет:</label><input type="text" name="subject" class="placeholder" placeholder="Предмет">
 	<label for="username">Задание</label><textarea input type="text" name="hometask" class="placeholder" placeholder="Задание"></textarea>
