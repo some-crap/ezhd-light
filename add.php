ae982ad87dde57ab2f6ba89b8b487405886b79df
some-crap <43110604+some-crap@users.noreply.github.com>
2018-09-15 18:04:57 +0300

    Обновление безопасности

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php
 include 'config.php';
 include 'response.php';
-if(isset($_POST['timestamp']) && isset($_POST['subject']) && isset($_POST['hometask'])) {
+if(isset($_POST['admin_token']) && isset($_POST['timestamp']) && isset($_POST['subject']) && isset($_POST['hometask'])) {
 	$mysqli = new mysqli(HOST, USERNAME, PASS, DBNAME);
 	
 	$date = trim($_POST['timestamp']);
@@ -11,6 +11,10 @@ if(isset($_POST['timestamp']) && isset($_POST['subject']) && isset($_POST['homet
 	
 	if(empty($timestamp) || empty($subject) || empty($hometask)) {
 		onError(1);
+		exit();
+	}
+	else if(!(strcmp($_POST['admin_token'], ADMIN_TOKEN) == 0)) {
+		onError(2);
 		exit();
 	}
 	
