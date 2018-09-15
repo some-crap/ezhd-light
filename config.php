df2badfc78e73e11ec269ef049449b553e516a05
some-crap <43110604+some-crap@users.noreply.github.com>
2018-09-15 09:58:26 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-const HOST     =  "localhost";
-const USERNAME =  "DBUSER";
-const PASS     =  "DBPASS";
-const DBNAME   =  "DB";
-
-//Secure key which give access to adding / editing / deleting homework
-const ADMIN_TOKEN =  "Key";
-?>
