76260aea3d4ef9b667695b8f787d8d2a551fcd1f
some-crap <yuramatveev03@ya.ru>
2018-09-18 14:41:02 +0300

    Переписал авторизацию и админ-панель

--- check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$check = $_SESSION['name'];
-echo'Привет, ';
-echo $check;
-	header("HTTP/1.1 301 Moved Permanently");
-    header('Refresh: 2; url=../');
-?>
--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST','localhost');
-define('USERNAME','');
-define('PASS','');
-define('DBNAME','');
-
-//ключ для бота
-const ADMIN_TOKEN =  "";
-?>
